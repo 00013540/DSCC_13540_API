9dacdf6b64424e158cf217a36c18b88d8265a1b8
00013540 <wiut00013540@gmail.com>
2024-10-29 17:01:52 +0500

    Models developed

---
+++ DSCC_13540_API/Models/Author.cs
@@ -0,0 +1,11 @@
+﻿namespace DSCC_13540_API.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Bio { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public ICollection<Book> Books { get; set; }
+    }
+}
---
+++ DSCC_13540_API/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace DSCC_13540_API.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+    }
+}
