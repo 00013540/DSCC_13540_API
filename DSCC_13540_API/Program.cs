ab43b6d95282b55222135cd0dc78f9788d9db2dd
00013540 <wiut00013540@gmail.com>
2024-10-30 10:53:50 +0500

    Project setup

--- DSCC_13540_API/Program.cs
+++ DSCC_13540_API/Program.cs
@@ -1,6 +1,28 @@
+using DSCC_13540_API.Data;
+using DSCC_13540_API.Interfaces;
+using DSCC_13540_API.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<LibraryContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IAuthorRepository, AuthorRepository>();
+builder.Services.AddScoped<IBookRepository, BookRepository>();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("https://localhost:7139")
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
