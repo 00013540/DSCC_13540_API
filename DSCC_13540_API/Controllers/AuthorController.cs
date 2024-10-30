7549d7051fff162da72102e38561ea8a2d6be8e1
00013540 <wiut00013540@gmail.com>
2024-10-30 10:56:19 +0500

    Controllers developed and tested

---
+++ DSCC_13540_API/Controllers/AuthorController.cs
@@ -0,0 +1,80 @@
+﻿using AutoMapper;
+using DSCC_13540_API.DTOs;
+using DSCC_13540_API.Interfaces;
+using DSCC_13540_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DSCC_13540_API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        private readonly IAuthorRepository _authorRepository;
+        private readonly IMapper _mapper;
+
+        public AuthorController(IAuthorRepository authorRepository, IMapper mapper)
+        {
+            _authorRepository = authorRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AuthorDto>>> GetAllAuthors()
+        {
+            var authors = await _authorRepository.GetAllAsync();
+            return Ok(_mapper.Map<IEnumerable<AuthorDto>>(authors));
+        }
+
+        [HttpGet("{id}", Name = "GetAuthorById")]
+        public async Task<ActionResult<AuthorDto>> GetAuthorById(int id)
+        {
+            var author = await _authorRepository.GetByIdAsync(id);
+            if (author == null)
+            {
+                return NotFound();
+            }
+            return Ok(_mapper.Map<AuthorDto>(author));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<AuthorDto>> CreateAuthor(AuthorCreateDto authorCreateDto)
+        {
+            var author = _mapper.Map<Author>(authorCreateDto);
+            await _authorRepository.AddAsync(author);
+
+            var authorDto = _mapper.Map<AuthorDto>(author);
+
+            return CreatedAtRoute("GetAuthorById", new { id = authorDto.Id }, authorDto);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateAuthor(int id, AuthorCreateDto authorUpdateDto)
+        {
+            var author = await _authorRepository.GetByIdAsync(id);
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            _mapper.Map(authorUpdateDto, author);
+            await _authorRepository.UpdateAsync(author);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteAuthor(int id)
+        {
+            var author = await _authorRepository.GetByIdAsync(id);
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            await _authorRepository.DeleteAsync(id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ DSCC_13540_API/Controllers/BookController.cs
@@ -0,0 +1,80 @@
+﻿using AutoMapper;
+using DSCC_13540_API.DTOs;
+using DSCC_13540_API.Interfaces;
+using DSCC_13540_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DSCC_13540_API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+        private readonly IMapper _mapper;
+
+        public BookController(IBookRepository bookRepository, IMapper mapper)
+        {
+            _bookRepository = bookRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BookDto>>> GetAllBooks()
+        {
+            var books = await _bookRepository.GetAllAsync();
+            return Ok(_mapper.Map<IEnumerable<BookDto>>(books));
+        }
+
+        [HttpGet("{id}", Name = "GetBookById")]
+        public async Task<ActionResult<BookDto>> GetBookById(int id)
+        {
+            var book = await _bookRepository.GetByIdAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return Ok(_mapper.Map<BookDto>(book));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<BookDto>> CreateBook(BookCreateDto bookCreateDto)
+        {
+            var book = _mapper.Map<Book>(bookCreateDto);
+            await _bookRepository.AddAsync(book);
+
+            var bookDto = _mapper.Map<BookDto>(book);
+
+            return CreatedAtRoute("GetBookById", new { id = bookDto.Id }, bookDto);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateBook(int id, BookCreateDto bookUpdateDto)
+        {
+            var book = await _bookRepository.GetByIdAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _mapper.Map(bookUpdateDto, book);
+            await _bookRepository.UpdateAsync(book);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteBook(int id)
+        {
+            var book = await _bookRepository.GetByIdAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            await _bookRepository.DeleteAsync(id);
+
+            return NoContent();
+        }
+    }
+}
